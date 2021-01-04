91792cc538a386adfa15ceb0ab49ce34a80dad3e
huangshuo <huangshuo@lenovoCloud.com>
2021-01-04 12:17:49 +0800

    feat: 添加mysql数据库链接测试

---
+++ www/localhost/connect.php
@@ -0,0 +1,14 @@
+<?php
+$serverName = "mysql5";
+$userName = "root";
+$passWord = "123456";
+
+// 创建链接
+$conn = new mysqli($serverName, $userName, $passWord);
+
+// 检测链接
+if ($conn->connect_error) {
+    die("连接失败：" . $conn->connect_error);
+}
+echo "连接成功";
+?>
